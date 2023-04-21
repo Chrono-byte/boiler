ff3113f91acfe2014d85068aa27a138f5c3f59e4
Chrono <me@disilla.org>
2023-04-21 14:24:33 -0400

    fix

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -25,72 +25,70 @@ dotenv.config();
 // Create router
 const router = express.Router();
 
-// Login endpoint
-router.post(
-	"/login/email",
+router.get(
+	"/",
 	(
 		request: Request & {
 			authenticated: boolean;
-			query: { username: string; password: string };
+			query: { token: string };
 		},
 		res: Response
 	) => {
-		const username = request.query.username as string;
-		const password = request.query.password as string;
-
-		console.log({
-			username,
-			password,
-		});
-
-		// Check that username and password were provided
-		if (!username || !password) {
-			console.log({
-				username,
-				password,
+		// Check if user is authenticated
+		if (request.authenticated) {
+			// post
+			res.status(200).json({
+				authenticated: true,
+				username: request.query.username,
+				id: request.query.id,
 			});
-
-			res.status(400).json({ error: "Missing username or password" });
-			return;
+		} else {
+			// Send error
+			res.status(401).json({ error: "err" });
 		}
-
-		getUserByEmail(username)
-			.then(async (user: User) => {
-				// Check if password is correct
-				if ((await checkPassword(password, user.hash)) == true) {
-					// Generate token
-					const token = jwt.sign(
-						{
-							username: user.username,
-							id: user.id,
-							permissions: user.permissions,
-						},
-						process.env.JWT_SECRET,
-						{
-							expiresIn: "12h",
-						}
-					);
-
-					// Assign token to user
-					user.token = token;
-
-					// Send token
-					res.status(200).json({
-						token,
-						username: user.username,
-						id: user.id,
-					});
-				} else {
-					res.status(401).json({ error: "Incorrect password" });
-				}
-			})
-			.catch((error) => {
-				console.log(error);
-
-				return res.status(401).json({ error: "User does not exist" });
-			});
 	}
 );
+
+// Login endpoint
+router.post("/login/email", (request: Request, res: Response) => {
+	const { username, password } = request.body;
+
+	getUserByEmail(username)
+		.then(async (user: User) => {
+			// Check if password is correct
+			if ((await checkPassword(password, user.hash)) == true) {
+				// Generate token
+				const token = jwt.sign(
+					{
+						username: user.username,
+						id: user.id,
+						permissions: user.permissions,
+					},
+					process.env.JWT_SECRET,
+					{
+						expiresIn: "12h",
+					}
+				);
+
+				// Assign token to user
+				user.token = token;
+
+				// Send token
+				res.status(200).json({
+					token,
+					username: user.username,
+					id: user.id,
+				});
+			} else {
+				res.status(401).json({ error: "Incorrect password" });
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+
+			return res.status(401).json({ error: "User does not exist" });
+		});
+});
 
 // Register endpoint
 router.post(
